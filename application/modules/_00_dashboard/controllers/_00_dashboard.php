daf1d07f43f334f64121a3218f7ce60e3fdd66f9
vcsit2021 <91366489+selusindotproject@users.noreply.github.com>
2022-01-03 04:45:32 +0700

    custom dashboard

--- application/modules/_00_dashboard/controllers/_00_dashboard.php
+++ application/modules/_00_dashboard/controllers/_00_dashboard.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class _00_dashboard extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -21,8 +21,8 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		// $this->load->view('welcome_message');
-		$data['_view'] = 'welcome/welcome_message';
-        $data['_caption'] = '';
+		$data['_view'] = '_00_dashboard/_content';
+        $data['_caption'] = 'Dashboard';
         $this->load->view('_00_dashboard/_00_dashboard', $data);
 	}
 }
---
+++ application/modules/_00_dashboard/views/_main_header.php
@@ -0,0 +1,63 @@
+
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-light-cyan elevation-4">
+    <!-- Brand Logo -->
+    <a href="<?php echo site_url() ?>" class="brand-link">
+      <img src="<?php echo base_url() ?>assets/adminlte/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="<?php echo base_url() ?>assets/adminlte/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Alexander Pierce</a>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="<?php echo site_url() ?>" class="nav-link <?php echo $this->uri->segment(1) == '' ? 'active' : '' ?>">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="<?php echo site_url('welcome') ?>" class="nav-link <?php echo $this->uri->segment(1) == 'welcome' ? 'active' : '' ?>">
+              <i class="far fa-circle nav-icon"></i>
+              <p>Welcome</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
